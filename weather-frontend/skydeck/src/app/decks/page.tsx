9642ed9f67fccd2b7448e6454df3436a38627fd8
Zmyhrer <zachs.myhrer@gmail.com>
2025-05-07 18:42:13 -0500

    Removed Table and ElementValue Components

--- weather-frontend/skydeck/src/app/decks/page.tsx
+++ weather-frontend/skydeck/src/app/decks/page.tsx
@@ -5,41 +5,7 @@ import Table from "@/app/components/table";
 import dataDecks from "@/data/decks.json";
 
 const page = () => {
-  const first10Elements = dataDecks.data.map((item) => {
-    return {
-      ...item,
-      elements: item.elements.slice(0, 20),
-    };
-  });
-
-  const maxElementCount = Math.max(
-    ...first10Elements.map((item) => item.elements.length)
-  );
-  console.log(maxElementCount);
-  const elementNewHeaders = Array.from(
-    { length: maxElementCount },
-    (_, i) => `E${i + 1}`
-  );
-  const headerLabels = ["Deck Name", "Boolean", ...elementNewHeaders];
-  console.log(headerLabels);
-
-  return (
-    <div className="">
-      <div className="flex justify-between">
-        <h1 className="text-2xl font-semibold mb-4 flex justify-center md:justify-start  dark:bg-black dark:text-purple-50 ">
-          Decks
-        </h1>
-      </div>
-
-      <div className="border border-gray-200">
-        <Table
-          headersKey={dataDecks.headers}
-          headersLabel={headerLabels}
-          data={first10Elements}
-        />
-      </div>
-    </div>
-  );
+  return <div className=""></div>;
 };
 
 export default page;
